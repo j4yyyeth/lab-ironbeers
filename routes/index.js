cc0a9dc7dce66d78a1af4dc02558e83e1e1a775e
Jacob Handwerger <jstudyeth@gmail.com>
2023-02-01 17:59:34 -0500

    first commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index.hbs', { title: 'Home' });
+});
+
+router.get('/beers', function(req, res, next){
+  res.render('beers.hbs', { title: 'Beer'});
+  getBeers();
+});
+
+router.get('/random-beer', function(req, res, next) {
+  res.render('beers.hbs', { title: 'Random Beer' });
 });
 
 module.exports = router;
